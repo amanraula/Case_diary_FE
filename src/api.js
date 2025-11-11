65bfc615a29cc6a489dbbdd9aa7a03f77cf3c1fa
amanraula <amanraula2005@gmail.com>
2025-11-11 21:06:37 +0530

    url-deploy

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // change port if backend runs elsewhere
+  //baseURL: "http://localhost:5000/api", // change port if backend runs elsewhere
+  baseURL: "https://casedop.onrender.com/api",
 });
 
 export default api;
